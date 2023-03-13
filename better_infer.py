33425e5e57e243ca46339f1e0e071e2660e003fb
Tristan Charpentier <tristan_charpentier@hotmail.com>
2023-03-13 11:36:54 +0100

    begin xpu inference prep-work and better resilience

--- better_infer.py
+++ better_infer.py
@@ -12,7 +12,9 @@ from helpers.utils import NAME_TO_WIDTH, labels
 
 import ffmpeg
 
-float_formatter = "{:.5f}".format
+#import intel_extension_for_pytorch as ipex
+
+float_formatter = "{:.6f}".format
 np.set_printoptions(formatter={'float_kind':float_formatter})
 
 #target_dir = "/tmp/tri/testaudio/rising/"
@@ -26,7 +28,9 @@ def audio_tagging(args):
     Running Inference on an audio clip.
     """
     model_name = args.model_name
-    device = torch.device('cuda') if args.cuda and torch.cuda.is_available() else torch.device('cpu')
+    #device = torch.device('cuda') if args.cuda and torch.cuda.is_available() else torch.device('cpu')
+    #device = torch.device('xpu') if args.intel else torch.device('cpu')
+    device = torch.device('cpu')
     audio_path = args.audio_path
     sample_rate = args.sample_rate
     window_size = args.window_size
@@ -39,15 +43,19 @@ def audio_tagging(args):
     else:
         model = get_mobilenet(width_mult=NAME_TO_WIDTH(model_name), pretrained_name=model_name, strides=args.strides,
                               head_type=args.head_type)
-    model.to(device)
     model.eval()
+    #model = model.to(device)
+    #if args.cuda:
+    #    model = ipex.optimize(model)
 
     # model to preprocess waveform into mel spectrograms
     mel = AugmentMelSTFT(n_mels=n_mels, sr=sample_rate, win_length=window_size, hopsize=hop_size)
-    mel.to(device)
     mel.eval()
+    #model = model.to(device)
+    #if args.cuda:
+    #    model = ipex.optimize(model)
 
-    output_filename = "arjun_rating_ana_results.csv"
+    output_filename = "my_rating_ana_results.csv"
 
     audio_split = audio_path.split("/")
     if len(audio_split) > 1:
@@ -71,47 +79,53 @@ def audio_tagging(args):
         of.write("song name\t" + "\t".join(labels) + "\n")
 
 
-    print(prev)
-    found_names = os.listdir(audio_path)
-    found_names = [audio_path + f for f in found_names if ".m4a" in f and audio_path + f not in prev]
-
-    #of.write("song name\t" + "\t".join(labels) + "\n")
+    #print(prev)
+    found_names = [os.path.join(dp, f) for dp, dn, fn in os.walk(audio_path) for f in fn]#os.listdir(audio_path)
+    print(f"orig len: {len(found_names)}")
+    found_names = [f for f in found_names if (((".mp3" in f) or (".m4a" in f)) or ".flac" in f) and f not in prev]
+    print(f"now have {len(found_names)} songs")
+    print(found_names[0:10])
+    #print(found_names[0:10])
     count = 0
 
     for song_filename in found_names:
         print(f"----  COUNT {count} / {len(found_names)} -----")
         #(waveform, _) = librosa.core.load(song_filename, sr=sample_rate, mono=True)
-        rawaudio, _out  = (ffmpeg.input(song_filename).output('-', format='f32le', acodec='pcm_f32le', ac=1, ar='32000').run(capture_stdout=True, capture_stderr=True))
-        print("GOT HERE")
+        try:
+            rawaudio, _out = (ffmpeg.input(song_filename).output('-', format='f32le', acodec='pcm_f32le', ac=1, ar='32000').run(capture_stdout=True, capture_stderr=True))
+            print("GOT HERE")
 
-        audio = np.frombuffer(rawaudio, dtype=np.float32).copy()
-        waveform = torch.from_numpy(audio[None, :]).to(device)
+            if len(rawaudio) < 230400000:
+                audio = np.frombuffer(rawaudio, dtype=np.float32).copy()
+                waveform = torch.from_numpy(audio[None, :])#.to(device)
 
-        # our models are trained in half precision mode (torch.float16)
-        # run on cuda with torch.float16 to get the best performance
-        # running on cpu with torch.float32 gives similar performance, using torch.bfloat16 is worse
-        with torch.no_grad(), autocast(device_type=device.type) if args.cuda else nullcontext():
-            spec = mel(waveform)
-            preds, features = model(spec.unsqueeze(0))
-        preds = torch.sigmoid(preds.float()).squeeze().cpu().numpy()
+                # our models are trained in half precision mode (torch.float16)
+                # run on cuda with torch.float16 to get the best performance
+                # running on cpu with torch.float32 gives similar performance, using torch.bfloat16 is worse
+                with torch.no_grad(), autocast(device_type=device.type) if args.intel else nullcontext():
+                    spec = mel(waveform)
+                    preds, features = model(spec.unsqueeze(0))
+                preds = torch.sigmoid(preds.float()).squeeze().cpu().numpy()
 
-        #sorted_indexes = np.argsort(preds)[::-1]
+                #sorted_indexes = np.argsort(preds)[::-1]
 
-        # Print audio tagging top probabilities
-        #print("************* Acoustic Event Detected: *****************")
-        #for k in range(10):
-        #    print('{}: {:.3f}'.format(labels[sorted_indexes[k]],
-        #        preds[sorted_indexes[k]]))
-        #print("********************************************************")
+                # Print audio tagging top probabilities
+                #print("************* Acoustic Event Detected: *****************")
+                #for k in range(10):
+                #    print('{}: {:.3f}'.format(labels[sorted_indexes[k]],
+                #        preds[sorted_indexes[k]]))
+                #print("********************************************************")
 
-        dedup.append(song_filename)
-        out_str = song_filename + "\t"
+                dedup.append(song_filename)
+                out_str = song_filename + "\t"
 
-        #for k in range(len(labels)):
-        out_str += "\t".join([float_formatter(k) for k in preds])
-        out_str += "\n"
+                #for k in range(len(labels)):
+                out_str += "\t".join([float_formatter(k) for k in preds])
+                out_str += "\n"
 
-        of.write(out_str)
+                of.write(out_str)
+        except:
+            print(f"{song_filename} failed")
         count += 1
         #as_a_str = [float_formatter(k) for k in preds[sorted_indexes[k]]))]
     of.close()
@@ -124,7 +138,7 @@ if __name__ == '__main__':
     parser.add_argument('--model_name', type=str, default='mn40_as_ext')
     parser.add_argument('--strides', nargs=4, default=[2, 2, 2, 2], type=int)
     parser.add_argument('--head_type', type=str, default="mlp")
-    parser.add_argument('--cuda', action='store_true', default=False)
+    parser.add_argument('--intel', action='store_true', default=False)
     parser.add_argument('--audio_path', type=str, required=False)
 
     # preprocessing
